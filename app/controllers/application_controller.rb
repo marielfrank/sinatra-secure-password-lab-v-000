7801de4e1b59486449fb59475ba835a6df02c295
Mariel <marielbeth@gmail.com>
2017-09-14 16:29:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     @user = User.new(username: params[:username], password: params[:password])
 
-    puts @user.username
+    puts @user
     if @user.save
       redirect "/login"
     else
